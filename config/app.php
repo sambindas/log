9ad14f3b7b887e6ede9729888b241fbef0c10fd9
Samuel Bindas <bindas.fs@gmail.com>
2021-09-15 08:54:07 +0100

    add dtt

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -229,6 +230,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Consts' => App\Constants::class,
+        'Datatables' => Yajra\DataTables\Facades\DataTables::class,
 
     ],
 
