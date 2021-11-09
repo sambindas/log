0120a06196fc2f34e2514d59734ad55d389c31d6
Bindas Samuel MAC <bindas.fs@gmail.com>
2021-11-09 17:04:30 +0100

    new

--- resources/views/eclinic/adit.blade.php
+++ resources/views/eclinic/adit.blade.php
@@ -6,7 +6,6 @@
         <meta property="og:title" content="eClat">
         <meta property="og:description" content="">
         <meta property="og:image" content="https://res.cloudinary.com/dpiyqfdpk/image/upload/v1627104618/logo2_k3ibfh.svg">
-        <meta property="og:url" content="https://eclinic2.netlify.app">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous" />
 <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css" integrity="sha512-17AHGe9uFHHt+QaRYieK7bTdMMHBMi8PeWG99Mf/xEcfBLDCn0Gze8Xcx1KoSZxDnv+KnCC+os/vuQ7jrF/nkw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -15,7 +14,9 @@
         <link rel="shortcut icon" href="../assets/images/logo.svg" type="image/x-icon" />
         <link rel="stylesheet" href="../assets/css/incident.css">
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="{{asset('assets/deji/assets/fonts/custom/stylesheet.css')}}">
+        
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
         <style type="text/css">
             .modal {
                 display: block;
@@ -79,7 +80,7 @@
 
                     <div class="area">
                         <label for="">Incident description</label>
-                        <textarea name="issue" id="issue"  rows="5" placeholder="Write a description of the incident encountered here.." ></textarea>
+                        <textarea name="issue" id="summernotes"  rows="5" placeholder="Write a description of the incident encountered here.." ></textarea>
                     </div>
 
                     <div class="groupFlex">
@@ -126,14 +127,14 @@
 
     </div>
     <script src="{{asset('assets/js/jquery-3.5.1.min.js')}}"></script>
-    <script src="../assets/scripts/incident.js"></script>
-    <script src="../assets/scripts/pagination.js"></script>
-    <script src="../assets/scripts/header.js"></script>
-    <script src="../assets/scripts/modal.js"></script>
-    <script src="../assets/scripts/detail.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
    
     <script type="text/javascript">
+        $(document).ready(function() {
+            $('#summernotes').summernote();
+        });
         $(document).ready(function() {
             $('.sl2').select2({
                 theme: "classic",
